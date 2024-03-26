298ba9a884c3e74a335789e32970a550a204190b
Bruno Dias <brunogd148@gmail.com>
2024-03-26 23:19:56 +0000

    feat: Ex23(Completo)

--- src/main/java/algoritimos/java/Exercicio23.java
+++ src/main/java/algoritimos/java/Exercicio23.java
@@ -3,11 +3,35 @@ import java.util.Scanner;
 
 public class Exercicio23 {
     public static void main(String[] args) {
-        int vet[]; 
         Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < 10; i++){
-            System.out.println("Digite o " + (i+1) + "º elemento da soma:");
-            vet[i] = scanner.nextInt();
+        int[] vetor = new int[10];
+
+        System.out.println("Digite os 10 números a serem somados:");
+
+        preencherVetor(vetor, 0, scanner);
+
+        int soma = calcularSoma(vetor, 0);
+
+        System.out.println("A soma de todos os números é: " + soma);
+
+        scanner.close();
+    }
+
+    public static void preencherVetor(int[] vetor, int indice, Scanner scanner) {
+        if (indice < vetor.length) {
+            System.out.print("Digite o " + (indice + 1) + "º número: \n");
+            vetor[indice] = scanner.nextInt();
+            preencherVetor(vetor, indice + 1, scanner);
+        }
+    }
+
+    // Função recursiva para calcular a soma dos elementos do vetor
+    public static int calcularSoma(int[] vetor, int indice) {
+        if (indice == vetor.length - 1) {
+            return vetor[indice];
+        } else {
+            return vetor[indice] + calcularSoma(vetor, indice + 1);
         }
     }
 }
+
